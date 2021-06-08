bb8c5e27a29e918055095da2bc0771b2b26cfcd0
Tsuyoshi Akiyama <ka090824@gmail.com>
2021-06-08 17:22:35 +0900

    feat: add twist controller init to set up helper controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,3 +1,7 @@
+from lowpass import LowPassFilter
+from yaw_controller import YawController
+from pid import PID
+import rospy
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
@@ -6,9 +10,14 @@ ONE_MPH = 0.44704
 class Controller(object):
     def __init__(self, *args, **kwargs):
         # TODO: Implement
-        pass
+        self.yaw_controller = YawController()
+        self.throttle_controller = PID()
+        self.vel_lpf = LowPassFilter()
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
+    def control(self, dbw_enabled, current_vel, linear_vel, angular_vel):
         # Return throttle, brake, steer
+        if not dbw_enabled:
+            self.throttle_controller.reset()
+            return 0., 0., 0.
+
         return 1., 0., 0.
